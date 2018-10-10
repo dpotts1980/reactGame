cf00535e9a4684468bb9f2e875c1ecf8050711f8
Daniel Potts <drp777@hotmail.com>
2018-10-09 21:06:08 -0500

    worked with Cameron to have cards rendering to page, need to adjust css and add handleClickEvent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 //import Navbar from './components/Navbar/Navbar.js';
 //import Modal from './components/Modal/Modal.js';
-//import CardsClick from './components/CardsClick/CardsClick.js';
+import CardsClick from './components/CardsClick';
 import cards from './cards.json';
 
 class App extends Component {
+  state = {
+    cards: cards
+  }
+ /*  handleClickEvent = () =>{
+    console.log("you Clicked!!!")
+  } */
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {this.state.cards.map( card => (
+          <CardsClick image={card.image} /* onClick={handleClickEvent()} */>{card.name}</CardsClick> 
+        )
+      )}
+      {/* <CardsClick image={cards[1].image}>Child 2</CardsClick> 
+      <CardsClick image={cards[2].image}>Child 3</CardsClick>  */}
+
+      {/* Cardsclick(); */}
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -23,7 +37,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
--- my-app/src/components/CardsClick/CardsClick.js
+++ my-app/src/components/CardsClick/CardsClick.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './CardsClick.css';
 
-function handleClick(props) {
-	props.reArrangeCards();
-	props.clickedBird(props.id);
+// function handleClick(props) {
+// 	props.reArrangeCards();
+// 	props.clickedBird(props.id);
+// }
+
+function Bla(name, age){
+	console.log(name)
 }
 
-function CardClick(props) {
-	return (
-		<div className="card img-container" onClick={() => handleClick(props)}>
-			<img alt={props.name} src={props.image} />
-		</div>
-	)
-}
-
-
+const CardClick = props => (
+	<div className="card img-container">
+		<img alt={props.name} src={props.image} />
+		{/* <h1>Color -> {props.color}</h1>
+		<h2>Child -> {props.children}</h2> */}
+	</div>
+)
 
 
 export default CardClick
